9c5d13153fe54a7e409934801d2cca72faf0786b
Genny Allcroft <gennyallcroft@hotmail.co.uk>
2019-06-21 20:57:08 +0000

    add final test

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -17,3 +17,9 @@ describe 'fizzbuzz' do
     expect(fizzbuzz(15)).to eq 'fizzbuzz'
   end
 end
+
+describe 'fizzbuzz' do
+  it 'returns "fizzbuzz" when passed 4' do
+    expect(fizzbuzz(4)).to eq 4
+  end
+end
